915282720ff2d8b9571dcf5a63e86c8f8161f328
Noah Evans <noah.evans@gmail.com>
2015-07-30 21:58:42 -0600

    initial commit of coq

---
+++ proof-general-coq.el
@@ -0,0 +1,19 @@
+;;; proof-general-coq.el ---
+
+;; Copyright 2015
+
+;; Author: Lots
+;; Maintainer: Noah Evans
+;; URL: https://github.com/npe9/company-coq
+;; Created: July 30 2015
+;; Version: 1.1.5
+;; Keywords: coq
+;; Package-Requires: ((company "0.5")
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY;
